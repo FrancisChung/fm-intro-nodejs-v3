fb90fa953d22d7628f7110d84be553a71fde7663
Francis Chung <francis.c.chung@gmail.com>
2025-01-08 13:36:30 +1100

    New "New" command

--- src/command.js
+++ src/command.js
@@ -12,8 +12,17 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 yargs(hideBin(process.argv))
-    .command('curl <url>', 'fetch the contents of the URL', () => {}, (argv) => {
-        console.info(argv)
-    })
+    .command('new <Note>', 'Create a new note', yargs  => {
+        return yargs.positional('note', {
+            type: 'string',
+            description: 'The content of the note to create',
+        })
+    }, (argv) => {console.info(argv)})
+    .option('tags', {
+        alias: 't',
+        type: 'string',
+        description: 'Tags to add to the note'
+    }
+)
     .demandCommand(1)
     .parse()
