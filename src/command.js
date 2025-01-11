311ec32aeba83a1d4882f47acce525a01e56c6db
Francis Chung <francis.c.chung@gmail.com>
2025-01-11 12:45:45 +1100

    all, find, remove, web, clean commands added

--- src/command.js
+++ src/command.js
@@ -22,7 +22,47 @@ yargs(hideBin(process.argv))
         alias: 't',
         type: 'string',
         description: 'Tags to add to the note'
-    }
-)
+    })
+    .command('all', 'get all notes', () => {}, async (argv) => {
+        const notes = await getAllNotes()
+        listNotes(notes)
+    })
+    .command('find <filter>', 'get matching notes', yargs => {
+        return yargs.positional('filter', {
+            describe: 'The search term to filter notes by, will be applied to note.content',
+            type: 'string'
+        })
+    }, async (argv) => {
+        const notes = await findNotes(argv.filter)
+        listNotes(notes)
+    })
+    .command('remove <id>', 'remove a note by id', yargs => {
+        return yargs.positional('id', {
+            type: 'number',
+            description: 'The id of the note you want to remove'
+        })
+    }, async (argv) => {
+        const id = await removeNote(argv.id)
+        if (id) {
+            console.log('Note removed: ', id)
+        } else {
+            console.log('Note not found')
+        }
+    })
+    .command('web [port]', 'launch website to see notes', yargs => {
+        return yargs
+            .positional('port', {
+                describe: 'port to bind on',
+                default: 5000,
+                type: 'number'
+            })
+    }, async (argv) => {
+        const notes = await getAllNotes()
+        start(notes, argv.port)
+    })
+    .command('clean', 'remove all notes', () => {}, async (argv) => {
+        await removeAllNotes()
+        console.log('All notes removed')
+    })
     .demandCommand(1)
     .parse()
