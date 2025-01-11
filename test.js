927dd200f29fb004412b76bb925c4ceee55c049f
Francis Chung <francis.c.chung@gmail.com>
2025-01-11 13:57:56 +1100

    Promise demo

--- test.js
+++ test.js
@@ -13,10 +13,26 @@
 //     console.log(data)
 // })
 
-const wait = (time, cb) => {
-    setTimeout(() => {
-        cb()
-    }, time)
+// const wait = (time, cb) => {
+//     setTimeout(() => {
+//         cb()
+//     }, time)
+// }
+//
+// wait(3000, () => console.log("3s hello world"))
+
+const wait2 = (time, cb) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, time)
+    })
 }
 
-wait(3000, () => console.log("3s hello world"))
+wait2(3000)
+    .then(() => {
+        return wait2(2000)
+    })
+
+await wait2(2000)
+await wait2(3000)
