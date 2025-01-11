f47979f467f51bb4b28f8e468bba816c5b99eb84
Francis Chung <francis.c.chung@gmail.com>
2025-01-11 13:48:34 +1100

    callback demo

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
-new Array(1000 * 20).fill(0).map((_, i) => {
-    console.log(i)
-})
+// new Array(1000 * 20).fill(0).map((_, i) => {
+//     console.log(i)
+// })
+
+
+// fs.readFile('file.txt').then(function (data) {
+//     console.log(data)
+// })
+//     .then(function (data) {
+//     console.log(data)
+// })
+//     .then(function (data) {
+//     console.log(data)
+// })
+
+const wait = (time, cb) => {
+    setTimeout(() => {
+        cb()
+    }, time)
+}
+
+wait(3000, () => console.log("3s hello world"))
