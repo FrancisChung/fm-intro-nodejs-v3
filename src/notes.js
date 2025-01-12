40e0c5245d15e1263655c96132ed9d48ae788755
Francis Chung <francis.c.chung@gmail.com>
2025-01-12 18:58:11 +1100

    RemoveNote, RemoveAllNotes

--- src/notes.js
+++ src/notes.js
@@ -19,3 +19,18 @@ export const findNotes = async (filter) => {
     const notes = await getAllNotes()
     return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
 }
+
+export const removeNote = async (id) => {
+    const notes = await getAllNotes()
+    const match = notes.find(note => note.id === id)
+
+    if (match) {
+        const newNotes = notes.filter(note => note.id !== id)
+        await saveDB({notes: newNotes})
+        return id
+    }
+}
+
+export const removeAllNotes = async () => {
+    await saveDB({notes: []})
+}
