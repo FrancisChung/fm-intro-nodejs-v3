bfa4ddc5f496bccc7c1512ec6d8d19a9bfc80afe
Francis Chung <francis.c.chung@gmail.com>
2025-01-22 15:15:39 +1100

    New Note command

--- src/command.js
+++ src/command.js
@@ -10,6 +10,7 @@
 
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import {newNote} from "./notes.js";
 
 yargs(hideBin(process.argv))
     .command('new <Note>', 'Create a new note', yargs  => {
@@ -17,7 +18,11 @@ yargs(hideBin(process.argv))
             type: 'string',
             description: 'The content of the note to create',
         })
-    }, (argv) => {console.info(argv)})
+    }, async (argv) => {
+        const tags = argv.tags ? arvg.tags.split(',') : []
+        const note = await newNote(argv.note, tags)
+        console.log('New note! ', note)
+    })
     .option('tags', {
         alias: 't',
         type: 'string',
