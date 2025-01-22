e14bafceeafb0aa69716eca0df99c498095c4b4d
Francis Chung <francis.c.chung@gmail.com>
2025-01-22 15:37:38 +1100

    Stupid typo fix

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 const DB_PATH = new URL("../db.json", import.meta.url).pathname
 
 export const getDB = async () => {
     const db = await fs.readFile(DB_PATH, 'utf-8')
-    return json.parse(db)
+    return JSON.parse(db)
 }
 
 export const saveDB = async (db) => {
