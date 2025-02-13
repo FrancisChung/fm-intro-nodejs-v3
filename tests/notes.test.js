026541615e216e01c28a138a72012f4aea956902
Francis Chung <francis.c.chung@gmail.com>
2025-02-13 14:15:50 +1100

    Jest Mocking Stub

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,7 +1,25 @@
-const add = (num, num2) => num + num2
+import {jest} from '@jest/globals'
 
-test('add takes 2 numbers and returns a sum', () => {
-    const result = add(1, 2)
+jest.unstable_mockModule('../src/db.js', () => ({
+    insertDB: jest.fn(),
+    getDB: jest.fn(),
+    saveDB: jest.fn(),
+}))
 
-    expect(result).toBe(3)
+const {insertDB, getDB, saveDB} = await import('../src/db.js')
+const {newNote, getAllNotes, removeNote } = await import('../src/notes.js')
+
+beforeEach(() => {
+    insertDB.mockClear()
+    getDB.mockClear()
+    saveDB.mockClear()
 })
+
+
+// const add = (num, num2) => num + num2
+//
+// test('add takes 2 numbers and returns a sum', () => {
+//     const result = add(1, 2)
+//
+//     expect(result).toBe(3)
+// })
